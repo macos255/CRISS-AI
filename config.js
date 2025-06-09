2ee1eaf2ec01d4716ebae4dc2faa9a1b20d659f1
macos255 <djmacha255@gmail.com>
2025-06-09 21:24:40 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "CRISS-AI-UFYEHaIK#OcfAshqHe0ed1MgCFu-1MThqj4dobYpEGph1QNQcudo",
+SESSION_ID: process.env.SESSION_ID || "CRISS-AI-pAAWVJaT#Tg7kc9TZNMmBC9uLJENvrY8b08bqqKJQdsJRTBRfbTA",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
